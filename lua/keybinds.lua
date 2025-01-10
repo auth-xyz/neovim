6b75fc6640025327bb4b9adca70de1e439762bd4
auth-xyz <authenticover@gmail.com>
2025-01-10 10:07:33 -0400

    feat: Added a few more keybinds
    refactor:
    ~ Added some comments to explain the keybinds I added a bit more

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -10,11 +10,12 @@ vim.keymap.set("v", "<", "<gv", {}) -- <: Decrease indentation
 vim.keymap.set("v", ">", ">gv", {}) -- >: Increase indentation
 
 -- Buffer Navigation
-vim.keymap.set("n", "<tab>", ":bnext<CR>")                       -- <Tab>: Switch to the next buffer
+vim.keymap.set("n", "<tab>", ":bnext<CR>") -- <Tab>: Switch to the next buffer
 vim.keymap.set("n", "<leader>x", ":bp<bar>sp<bar>bn<bar>bd<CR>") -- <Leader>x: Close the current buffer
 
 -- Toggle highlight search
 vim.keymap.set("n", "<leader>h", ":set hlsearch!<CR>")
+vim.api.nvim_set_keymap("n", "<leader>E", ":lua vim.diagnostic.setqflist()<CR>", { noremap = true, silent = true })
 
 -- Plugin Keybindings
 -- Codeium
@@ -23,7 +24,7 @@ vim.keymap.set("i", "<C-g>", function()
 end, { expr = true, silent = true }) -- <Ctrl-g>: Accept Codeium suggestion
 
 -- FTerm
-vim.keymap.set("n", "<leader>tt", ":lua require('FTerm').toggle()<CR>")            -- <Leader>tt: Toggle FTerm (Normal mode)
+vim.keymap.set("n", "<leader>tt", ":lua require('FTerm').toggle()<CR>") -- <Leader>tt: Toggle FTerm (Normal mode)
 vim.keymap.set("t", "<leader>tt", "<C-\\><C-n>:lua require('FTerm').toggle()<CR>") -- <Leader>tt: Toggle FTerm (Terminal mode)
 
 -- Neo-tree toggle
@@ -32,17 +33,16 @@ vim.keymap.set("n", "<leader>e", ":Neotree<CR>")
 -- Comment Toggle
 vim.keymap.set({ "n", "v" }, "<leader>/", ":CommentToggle<CR>") -- <Leader>/: Toggle comment
 
-
 -- LSP Keybindings
-vim.keymap.set("n", "<leader>ff", ":lua vim.lsp.buf.format()<CR>")        -- <Leader>ff: Format the current buffer
-vim.keymap.set("n", "K", vim.lsp.buf.hover, {})                           -- K: Show hover documentation
-vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})                     -- gd: Go to definition
-vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})   -- <Leader>ca: Code actions (normal/visual mode)
-vim.keymap.set("n", "<leader>m", vim.lsp.buf.rename, {})                  -- <Leader>m: Rename symbol
+vim.keymap.set("n", "<leader>ff", ":lua vim.lsp.buf.format()<CR>") -- <Leader>ff: Format the current buffer
+vim.keymap.set("n", "K", vim.lsp.buf.hover, {}) -- K: Show hover documentation
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, {}) -- gd: Go to definition
+vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {}) -- <Leader>ca: Code actions (normal/visual mode)
+vim.keymap.set("n", "<leader>m", vim.lsp.buf.rename, {}) -- <Leader>m: Rename symbol
 
 -- Themery keybinds, set default to gruvbox-material
 vim.keymap.set("n", "<leader>th", ":Themery<CR>")
 
--- Spectacle keybinds 
-vim.api.nvim_set_keymap("n", "<leader>ts", "<cmd>:lua require('spectacle').SpectacleTelescope()<CR>",{})
+-- Spectacle keybinds
+vim.api.nvim_set_keymap("n", "<leader>ts", "<cmd>:lua require('spectacle').SpectacleTelescope()<CR>", {})
 vim.api.nvim_set_keymap("n", "<leader>ss", "<cmd>:lua require('spectacle').SpectacleSave()<CR>", {})
